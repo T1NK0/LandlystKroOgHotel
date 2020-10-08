23151c64770e9e7a84018000946ae0ac477f4450
Mads Tinko Madsen <Lyderslev1@gmail.com>
2020-10-08 20:14:32 +0200

    Can create customer.

--- LandlystKroOgHotel/Classes/BookingManager.cs
+++ LandlystKroOgHotel/Classes/BookingManager.cs
@@ -8,9 +8,8 @@ namespace LandlystKroOgHotel.Classes
     public class BookingManager
     {
         SQL sql = new SQL();
-        //public List<Customer> customers = new List<Customer>();
 
-        public /*Customer*/ void CreateCustomer(string UIFirstname, string UILastname, string UIAddress, string UIPostalNumb, string UICity, string UITelephone, string UIEmail)
+        public void CreateCustomer(string UIFirstname, string UILastname, string UIAddress, string UIPostalNumb, string UICity, string UITelephone, string UIEmail)
         {
             Customer addCustomer = new Customer(UIFirstname, UILastname, UIAddress, UIPostalNumb, UICity, UITelephone, UIEmail);
             sql.CreateCustomer(addCustomer.FirstName, addCustomer.LastName, addCustomer.Address, addCustomer.PostalNumber, addCustomer.City, addCustomer.TelephoneNumber, addCustomer.Email);
